b0ef19970edc8ed069619a03d4bcd05c6ae6b42d
Jason D'Souza <jasondsouza2003@gmail.com>
2024-04-12 04:40:12 +0800

    Reorganized the "/src" directory

--- compile.js
+++ compile.js
@@ -1,23 +1,31 @@
 const ts = require('typescript');
 const fs = require('fs');
+const path = require('path');
 
-let files = ['types', 'algorithms', 'entities', 'engine', process.argv.length > 2 ? process.argv[2] : 'adventure_kingdom'];
+let folder = process.argv.length > 2 ? process.argv[2] : 'adventure_kingdom';
+let files = ['game.ts'];//'types', 'algorithms', 'entities', 'engine', process.argv.length > 2 ? process.argv[2] : 'adventure_kingdom'];
 let cont = [];
 
-process.stdout.write(`${files[files.length-1]}.ts\t\t`);
 
 async function watch() {
-    let changed = -1;
+    let changed = -1, line = -1;
     for(var i = 0; i < files.length; i++) {
-        let tmp = (await fs.promises.readFile(`src/${files[i]}.ts`)).toString('utf8');
+        let tmp = (await fs.promises.readFile(path.resolve('src',folder,files[i]))).toString('utf8');
         if (tmp != cont[i]) {
+            let diff = [cont[i].split('\n'), tmp.split('\n')];
+            for(let j = 0; j < diff[0].length; j++) if (diff[0][j] != diff[1][j]) {
+                line = j;
+                break;
+            }
+            if (line == -1) line = diff[0].length;
             cont[i] = tmp;
             changed = i;
             break;
         }
     }
     if (changed != -1) {
-        process.stdout.write(`${files[changed]}.ts\t\t`);
+        let d = new Date();
+        process.stdout.write(`${files[changed]} in line ${line+1} at ${d.getHours()}:${d.getMinutes()} `);
         compile();
     } else setTimeout(watch, 1000);
 }
@@ -31,21 +39,23 @@ async function compile() {
         removeCmments: true
     };
     try {
-        let code = '';
-        files.forEach((f,i) => code += cont[i].replace(/(import |export ).*/g, ''));
+        let code = [cont[0], ...cont[0].match(/import \{.*\} from ".*";/g)].reduce((code,match)=>code.replace(match, cont[files.indexOf(match.match(/(?<=import \{.*\} from ").*(?=";)/)[0])].replace(/(import |export |\/\*#\*\/ ).*/g, '')));
         const scriptjs = ts.transpileModule(code, options).outputText;
         const web = `<!DOCTYPE html><html><head><title>Adventure Kingdom</title><style>body{background-color:#000}canvas{position:fixed;left:0;top:0;width:100%;height:100%;object-fit:contain;image-rendering:pixelated;z-index:1}h1{z-index:2;opacity:0}</style></head><body><script>${scriptjs}</script></body></html>`
         await fs.promises.writeFile('index.html', web);
-        process.stdout.write(`Success\n`);
+        process.stdout.write(`(Success)\n`);
         watch();
     } catch(e) {
-        process.stdout.write(`Failed\n`);
+        process.stdout.write(`(Failed ${e})\n`);
     }
 }
 
 async function init() {
     try {
-        for(var i = 0; i < files.length; i++) cont[i] = (await fs.promises.readFile(`src/${files[i]}.ts`)).toString('utf8');
+        process.stdout.write(`${files[files.length-1]}\t\t`);
+        cont.push((await fs.promises.readFile(`src/${folder}/game.ts`)).toString('utf8'));
+        files.push(...cont[0].match(/(?<=import \{.*\} from ").*(?=";)/g)); //.map(x=>)
+        for (var i = 1; i < files.length; i++) cont[i] = (await fs.promises.readFile(path.resolve('src',folder,files[i]))).toString('utf8');
         compile();
     } catch (err) {
         console.log('Error reading files: ', err);
--- src/adventure_kingdom/game.ts
+++ src/adventure_kingdom/game.ts
@@ -1,31 +1,36 @@
-import {engine} from "./engine.ts";
+import {algo} from "../algorithms.ts";
+import {required_files} from "./entities.ts";
+import {engine} from "../engine.ts";
 
-let main = new engine({
-    z:10, w:160, h:120/*,
-    sprite_boxed:true,
-    hitbox_boxed:true,
-    rotate_boxed:true*/
-});
-let player = main.entity('knight', {y:88});
+let main = new engine({z:10, w:160, h:120});
+let player = main.entity('knight', {y:0});
 let clouds = main.entities('eud', Math.random()*10);
 let back = main.entities('eud', 5, {x:-8*3,y:main.h,w:main.w/8+4,t:1,hp:Math.floor(Math.random()*3+2)})
 let water = main.entity('liquid', {x:13*8, y:104, w:7, h:2, lava:false})
 let dirts = [
     main.entity('dirt', {x:0,y:104,w:14,h:2}),
-    main.entity('dirt', {x: 13*8-2*8, y:104-8*3, w:9, h:5})
+    main.entity('dirt', {x: 13*8-5*8, y:104-8*3, w:12, h:5}),
+    main.entity('dirt', {x:10*8,y:104-12*3,w:3,h:9})
 ];
+dirts[2].hitbox[0] = 15;
 player.collide = dirts;
-main.load('sprites.png');
+main.load(...required_files);
 main.scene('menu', (t, dt) => {
     if (dt > 100) return;
     main.draw('', {color: 'skyblue'});
     main.add(clouds, back);
     main.draw('', {alpha:0.2, color:'#dddddd'});
 
-    main.add(dirts[1], water, dirts[0], player);
+    main.add(dirts[2], dirts[1], water, dirts[0], player);
 
     if(player.sword < 0.01) main.on('Enter', e=> {
         if (e.init) player.sword = 1;
+    });
+    main.on('z', e=> {
+        if (e.init) main.hitbox_boxed = main.sprite_boxed = main.rotate_boxed = !main.sprite_boxed;
+    });
+    main.on('h', e=> {
+        if (e.init) main.hitbox_boxed = !main.hitbox_boxed;
     });
     if (main.on('w, ,ArrowUp')) {
         player.crouched = true;
@@ -34,7 +39,7 @@ main.scene('menu', (t, dt) => {
         player.crouched = true;
         player.jumping = false;
     } else {
-        if (player.crouched && player.on_ground && player.jumping) player.m[1] = 12;
+        if (player.crouched && player.ground && player.jumping) player.m[1] = 12;
         player.crouched = player.jumping = false;
     }
     if(main.on('d,ArrowRight')) player.m[0] += (3-player.m[0])*dt/300;
