9821d68e377e38e291d656990289c1e765e3e2f1
Jasouza <jasondsouza2003@gmail.com>
2024-04-17 05:02:26 +0000

    Added text entity

--- src/aswang_king/levels.ts
+++ src/aswang_king/levels.ts
@@ -23,6 +23,11 @@ let level = main => [
         main.entity('pressure_plate', {x:15*4, y:main.h-16-2, w:10, bind: [
             main.entity('shooter', {x:16*8-4, y:main.h-16*3, a:Math.PI, s:25}),
         ]}),
+        main.entity('text', {text:'Level 1 - Tutorial'}),
+
+
+
+        main.entity('plat', {x:40*16, y:main.h-16, w:16, h:2, dropoff:false}),
         /*/main.entity('vine',{y:10,h:20}),
         main.entity('wire',{y:main.h-16*5,h:30, bind:[
             main.entity('shooter',{y:main.h-16*4,s:50,a:Math.PI*0.25})
