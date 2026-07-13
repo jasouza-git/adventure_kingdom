327f12c7601f8a4ad229102a3501c44e01c73d5d
CpE21 <CpELab21@CpELab.local>
2026-07-13 12:22:46 +0800

    Reposition Merchant Board Y spawn to entity.y + 35 to ground signpost correctly

--- src/aswang_king/game.ts
+++ src/aswang_king/game.ts
@@ -75,7 +75,7 @@ function setupPlatforms() {
                 
                 // Spawn shop next to checkpoints index 17 (Level 2 start), 33 (Level 3 start), and 41 (Boss checkpoint)
                 if (index === 17 || index === 33 || index === 41) {
-                    itemsToAdd.push(main.entity('merchant_board', {x: entity.x + 15, y: entity.y + 3}));
+                    itemsToAdd.push(main.entity('merchant_board', {x: entity.x + 15, y: entity.y + 35}));
                     if (index === 17) {
                         itemsToAdd.push(main.entity('mysterious_person', {x: entity.x + 100, y: entity.y + 3}));
                         itemsToAdd.push(main.entity('albularyo', {x: entity.x + 150, y: entity.y + 3}));
